73558806e2c3df74d95bed581f9494debc337c4b
Klopenko Ruslan Aleksandrovich <balista15e@yandex.ru>
2019-05-13 18:59:55 +0300

    Убрал мусор, заглушил предупреждение

--- application/part_1_2_php/components/Autoloader.php
+++ application/part_1_2_php/components/Autoloader.php
@@ -8,6 +8,7 @@ class Autoloader
     public static function init()
     {
         spl_autoload_register(function ($className) {
+            /** @noinspection PhpIncludeInspection */
             include $className . '.php';
         });
     }
